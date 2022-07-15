7168dced7740aa0a0b9db260b31ba5e0fc6d8010
Eugene I <eugene@mitocgroup.com>
2022-07-15 08:18:43 -0400

    minor bugfix

--- bin/cicd-registry.sh
+++ bin/cicd-registry.sh
@@ -30,27 +30,6 @@ git clone https://github.com/fintechless/ftl-install
 git clone https://github.com/fintechless/ftl-api
 git clone https://github.com/fintechless/ftl-mgr
 
-_run ${REPOS}/ftl-api ${CWD} ${LWD}
-_run ${REPOS}/ftl-msa-msg-in ${CWD} ${LWD}
-_run ${REPOS}/ftl-msa-msg-out ${CWD} ${LWD}
-_run ${REPOS}/ftl-msa-msg-pacs-008 ${CWD} ${LWD}
-_run ${REPOS}/ftl-msa-rmq-in ${CWD} ${LWD}
-_run ${REPOS}/ftl-msa-rmq-out ${CWD} ${LWD}
-
-cd ${REPOS}/ftl-mgr
-if [ -f "${CWD}/bin/cicd-pipeline.sh" ]; then
-  echo "[EXEC] /bin/bash ${CWD}/bin/cicd-pipeline.sh"
-  export CODEBUILD_SRC_DIR=$(pwd -P) && export CICD_THUB_BUILD=true && /bin/bash "${CWD}/bin/cicd-pipeline.sh"
-else
-  echo "[EXEC] /bin/bash ${LWD}/cicd-pipeline.sh"
-  export CODEBUILD_SRC_DIR=$(pwd -P) && export CICD_THUB_BUILD=true && /bin/bash "${LWD}/cicd-pipeline.sh"
-fi || { echo >&2 "[ERROR] Failed to run 'cicd-pipeline.sh'. Aborting..."; exit 1; }
-
-cd ${REPOS}
-for i in $(ls -d */ | grep ftl-msa); do
-  aws s3 sync --exclude "^\." ./${i%%/} s3://${FTL_RUNTIME_BUCKET}/git/fintechless/${i%%/}/main
-done
-
 _run()
 {
   cd ${1}
@@ -79,6 +58,27 @@ _run()
     export CODEBUILD_SRC_DIR=$(pwd -P) && export CICD_THUB_INCLUDE=aws_ && /bin/bash "${3}/cicd-pipeline.sh"
   fi || { echo >&2 "[ERROR] Failed to run 'cicd-pipeline.sh'. Aborting..."; exit 1; }
 }
+
+_run ${REPOS}/ftl-api ${CWD} ${LWD}
+_run ${REPOS}/ftl-msa-msg-in ${CWD} ${LWD}
+_run ${REPOS}/ftl-msa-msg-out ${CWD} ${LWD}
+_run ${REPOS}/ftl-msa-msg-pacs-008 ${CWD} ${LWD}
+_run ${REPOS}/ftl-msa-rmq-in ${CWD} ${LWD}
+_run ${REPOS}/ftl-msa-rmq-out ${CWD} ${LWD}
+
+cd ${REPOS}/ftl-mgr
+if [ -f "${CWD}/bin/cicd-pipeline.sh" ]; then
+  echo "[EXEC] /bin/bash ${CWD}/bin/cicd-pipeline.sh"
+  export CODEBUILD_SRC_DIR=$(pwd -P) && export CICD_THUB_BUILD=true && /bin/bash "${CWD}/bin/cicd-pipeline.sh"
+else
+  echo "[EXEC] /bin/bash ${LWD}/cicd-pipeline.sh"
+  export CODEBUILD_SRC_DIR=$(pwd -P) && export CICD_THUB_BUILD=true && /bin/bash "${LWD}/cicd-pipeline.sh"
+fi || { echo >&2 "[ERROR] Failed to run 'cicd-pipeline.sh'. Aborting..."; exit 1; }
+
+cd ${REPOS}
+for i in $(ls -d */ | grep ftl-msa); do
+  aws s3 sync --exclude "^\." ./${i%%/} s3://${FTL_RUNTIME_BUCKET}/git/fintechless/${i%%/}/main
+done
 
 ###########################################
 ### DO NOT ADD COMMANDS AFTER THIS LINE ###
